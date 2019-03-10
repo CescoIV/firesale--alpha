268e14395e9e76c60223729a1455228eb733a356
Cesco <crordaz@gmail.com>
2019-03-09 21:36:48 -0600

    finished base CRUD routes

--- server/api/inventory.js
+++ server/api/inventory.js
@@ -15,13 +15,15 @@ var inventoryDB = new Datastore({
     autoload: true,
 });
 
+/*
+    GET REQUESTS
+*/
+
 //GET inventory
 app.get("/", (req,res)=>{
     res.send("Inventory API");
 });
-
 //GET a product from inventor by _id
-
 app.get("/product/:productId", (req,res)=>{
     if(!req.params.productId){
         res.status(500).send("ID field is required");
@@ -31,11 +33,86 @@ app.get("/product/:productId", (req,res)=>{
         });
     }
 });
-
 //GET all inventory products
-
 app.get("/products", (req,res)=>{
     inventoryDB.find({}, (err, items)=>{
         res.send(items);
     })
 });
+
+/*
+    POST REQUESTS
+*/
+
+// Create inventory product
+
+app.post("/product", (req,res)=>{
+    let newProduct = req.body;
+
+    inventoryDB.insert(newProduct, (err,item)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.send(item);
+        }
+    });
+});
+
+/*
+    DELETE REQUESTS
+*/
+
+//Delete inventory product
+
+app.delete("/product/:productId", (req,res)=>{
+    inventoryDB.remove({_id: req.params.productId},(err,itemRemoved)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+/*
+    PUT REQUESTS
+*/
+
+//Update an inventory product
+
+app.put("/product",(req,res)=>{
+    let id = req.body._id;
+
+    inventoryDB.update({_id: id}, req.body, {}, (err, numReplaced, item)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(item);
+        }
+    });
+});
+
+// app.decrementInventory = function(products) {
+//     async.eachSeries(products, function(transactionProduct, callback) {
+//       inventoryDB.findOne({ _id: transactionProduct._id }, function(
+//         err,
+//         product
+//       ) {
+//         // catch manually added items (don't exist in inventory)
+//         if (!product || !product.quantity_on_hand) {
+//           callback();
+//         } else {
+//           var updatedQuantity =
+//             parseInt(product.quantity_on_hand) -
+//             parseInt(transactionProduct.quantity);
+//           inventoryDB.update(
+//             { _id: product._id },
+//             { $set: { quantity_on_hand: updatedQuantity } },
+//             {},
+//             callback
+//           );
+//         }
+//       });
+//     });
+// };
